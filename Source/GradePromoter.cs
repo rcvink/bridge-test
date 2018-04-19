5cffa3f3c5779999298253f00e10676e8ad2047a
Michael Li Yan Hui <Mike.Hui@bridgeinternationalacademies.com>
2018-04-19 17:04:22 +0100

    minor code formatting

--- Source/GradePromoter.cs
+++ Source/GradePromoter.cs
@@ -20,16 +20,20 @@ namespace GradePromoter
     public void CalculatePromotions(string input, string output)
     {
         // Read csv file and transform them into ExamResult objects
-        var examResults = fileService.ParseExamResultsFromCsv(input);
+        var examResults = this.fileService.ParseExamResultsFromCsv(input);
 
-        // Get the a list of the distinct grade names from the ExamResult objects
-        var grades = examResults.Select(x => x.Grade ).Distinct().Select(x => new Grade { Name = x } ).ToList();
+        // Create a Grade list of the distinct grade names from the ExamResult objects
+        var grades = examResults
+                      .Select(x => x.Grade )
+                      .Distinct()
+                      .Select(x => new Grade { Name = x } )
+                      .ToList();
 
         // Process the exam results data
-        var pupils = promotionService.GetPromotionResults(examResults);
+        var pupils = this.promotionService.GetPromotionResults(examResults);
                   
         // Write the promotion results to a file
-        fileService.WritePromotionResults(grades, pupils, output);
+        this.fileService.WritePromotionResults(grades, pupils, output);
     }
   }
 }
